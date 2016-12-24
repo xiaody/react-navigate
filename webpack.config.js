cf5ad9b061ba22d86c1d5f1ea5cafbe832f6e29e
xiaody <youyounaihe@gmail.com>
2016-12-24 21:56:40 +0800

    fix webpack config typo

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   module: {
     rules: [
       {test: /\.jsx?$/, use: 'babel-loader'},
-      {test: /\.css?$/, use: ['style-loader', 'css-loader']}
+      {test: /\.css$/, use: ['style-loader', 'css-loader']}
     ]
   },
   performance: {
